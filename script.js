c9965e5134b74bcf8a00a98c2991c56db2b0264c
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-08-26 21:36:04 -0500

    seccion de skills

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
     });
 
-    // menu deslisante movile
-
-    $('.menu-btn').click(function(){
+    // toggle menu/navbar script
+    let movil = "fas fa-bars", cerra = "fa-solid fa-xmark";
+    $('.menu-btn').html(`<i class="${movil}"></i>`);
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
-            $('.menu-btn i').toggleClass("active");
+        $('.menu-btn').html(`<i class="${($(".navbar .menu").hasClass("active")) ? cerra : movil}"></i>`);
     });
 });
+
+
